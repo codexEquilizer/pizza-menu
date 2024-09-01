943f4f7950c93276d73bde2c458e5a40ab44f83c
Sourav Dutta <sourav.equilizer007@gmail.com>
2024-09-01 14:18:49 +0530

    passing and receiving props

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 
 const pizzaData = [
   {
@@ -48,27 +49,82 @@ const pizzaData = [
 
 function App() {
   return (
-    <div>
-      <h1>Hello React!</h1>
-      <Pizza />
-      <Pizza />
-      <Pizza />
+    <div className="container">
+      <Header />
+      <Menu />
+      <Footer />
     </div>
   );
 }
 
-function Pizza() {
+function Header() {
+  /* const style = {
+    color: "orangered",
+    fontSize: "48px",
+    textTransform: "uppercase",
+  }; */
+
   return (
-    <div>
-      <img src="pizzas/spinaci.jpg" alt="Pizza Spinaci" />
-      <h2>Pizza Spinaci</h2>
-      <p>Tomato, mozarella, spinach, and ricotta cheese</p>
+    <header className="header">
+      <h1>Fast React Pizza CO.</h1>;
+    </header>
+  );
+}
+
+const Menu = function () {
+  return (
+    <main className="menu">
+      <h2>Our Menu</h2>
+      <Pizza
+        name="Pizza Spinaci"
+        ingredients="Tomato, mozarella, spinach, and ricotta cheese"
+        photoName="pizzas/spinaci.jpg"
+        price="10"
+      />
+
+      <Pizza
+        name="Pizza Funghi"
+        ingredients="Tomato, mushrooms, blue cheese"
+        photoName="pizzas/funghi.jpg"
+        price="12"
+      />
+    </main>
+  );
+};
+
+function Pizza(props) {
+  console.log(props);
+  return (
+    <div className="pizza">
+      <img src={props.photoName} alt={props.name} />
+      <div>
+        <h3>{props.name}</h3>
+        <p>{props.ingredients}</p>
+        <span>{+props.price}</span>
+      </div>
     </div>
   );
 }
+
+const Footer = () => {
+  const hours = new Date().getHours();
+  const openHour = 12;
+  const closeHour = 22;
+  const isOpen = hours >= openHour && hours <= closeHour;
+  // console.log(isOpen);
+
+  return (
+    <footer className="footer">
+      {new Date().toLocaleTimeString()} We're currently{" "}
+      {isOpen ? "open" : "closed"}!
+    </footer>
+  );
+  // return React.createElement("footer", null, "We're currently open");
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
+  // StrictMode does not render any visible UI. It activates additional checks and warnings for its descendants.
   <React.StrictMode>
     <App />
   </React.StrictMode>
